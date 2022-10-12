f30466f274ac1c871510ba40bf3e1403de396752
delusion-dev <developer1.apriorit@gmail.com>
2022-10-12 19:31:17 +0300

    test

--- scripts/upload-android-to-sauce.sh
+++ scripts/upload-android-to-sauce.sh
@@ -5,4 +5,4 @@ curl \
   -F "name=$E2E_ANDROID_APP_NAME.apk" \
   -u "$SAUCE_USERNAME:$SAUCE_ACCESS_KEY" \
   'https://api.eu-central-1.saucelabs.com/v1/storage/upload'
-# TODO Check success
+# TODO Check success!
