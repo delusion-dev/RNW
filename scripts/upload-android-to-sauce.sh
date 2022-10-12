84a94cb383e545b97827b523da1fc893ce5004ac
delusion-dev <developer1.apriorit@gmail.com>
2022-10-12 19:12:23 +0300

    gen app names

--- scripts/upload-android-to-sauce.sh
+++ scripts/upload-android-to-sauce.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
-
+echo "Uploading Android app with the name: ${E2E_ANDROID_APP_NAME}.apk ..."
 curl \
   -F "payload=@./android/app/release/app-release.apk" \
-  -F "name=app-release-$APP_VERSION.apk" \
+  -F "name=$E2E_ANDROID_APP_NAME.apk" \
   -u "$SAUCE_USERNAME:$SAUCE_ACCESS_KEY" \
   'https://api.eu-central-1.saucelabs.com/v1/storage/upload'
+# TODO Check success
