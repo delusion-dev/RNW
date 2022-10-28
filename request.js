75d87f52c032b923f97d26f91a88e1c4689591b8
delusion-dev <developer1.apriorit@gmail.com>
2022-10-28 16:54:59 +0300

    Test Docker container

---
+++ request.js
@@ -0,0 +1,18 @@
+const http = require('http');
+
+http.get('http://localhost:5050/is_alive', (resp) => {
+  let data = '';
+
+  // A chunk of data has been received.
+  resp.on('data', (chunk) => {
+    data += chunk;
+  });
+
+  // The whole response has been received. Print out the result.
+  resp.on('end', () => {
+    console.log('result', data);
+  });
+
+}).on("error", (err) => {
+  console.log("Error: " + err.message);
+});
