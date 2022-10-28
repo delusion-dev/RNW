78bd7f9c089e766d9b35ef4cd6cd3133c7a36116
delusion-dev <developer1.apriorit@gmail.com>
2022-10-28 19:53:43 +0300

    test

--- request.js
+++ request.js
@@ -1,4 +1,5 @@
 const http = require('http');
+const request = require('request');
 
 http.get('http://localhost:5050/is_alive', (resp) => {
   let data = '';
@@ -16,3 +17,16 @@ http.get('http://localhost:5050/is_alive', (resp) => {
 }).on("error", (err) => {
   console.log("Error: " + err.message);
 });
+
+request.post(
+  'http://localhost:5050/mint',
+  { json: { 
+    "address": "0x04a79245c3b9bcb05af5c00a87b3608ad049100c523d308f7db4db35ac9663fb",
+    "amount": 50000000000000000
+   } },
+  function (error, response, body) {
+      if (!error && response.statusCode == 200) {
+          console.log(body);
+      }
+  }
+);
