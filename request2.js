43633f564d2b34c0ab39121c16ef8b819dbabd51
delusion-dev <developer1.apriorit@gmail.com>
2022-11-18 12:07:02 +0200

    test

--- request2.js
+++ request2.js
@@ -1,17 +1,45 @@
-const http = require('http');
-http.get('https://dapp.container/', (resp) => {
-  let data = '';
+var https = require('https');
 
-  // A chunk of data has been received.
-  resp.on('data', (chunk) => {
-    data += chunk;
+var options = {
+  host: 'dapp.container',
+  port: 443,
+  path: '/',
+  method: 'POST'
+};
+
+var req = https.request(options, function(res) {
+  console.log('STATUS: ' + res.statusCode);
+  console.log('HEADERS: ' + JSON.stringify(res.headers));
+  res.setEncoding('utf8');
+  res.on('data', function (chunk) {
+    console.log('BODY: ' + chunk);
   });
-
-  // The whole response has been received. Print out the result.
-  resp.on('end', () => {
-    console.log('result', data);
-  });
-
-}).on("error", (err) => {
-  console.log("Error: " + err.message);
 });
+
+req.on('error', function(e) {
+  console.log('problem with request: ' + e.message);
+});
+
+// write data to request body
+req.write('data\n');
+req.write('data\n');
+req.end();
+
+
+// const http = require('http');
+// http.get('http://dapp.container/', (resp) => {
+//   let data = '';
+
+//   // A chunk of data has been received.
+//   resp.on('data', (chunk) => {
+//     data += chunk;
+//   });
+
+//   // The whole response has been received. Print out the result.
+//   resp.on('end', () => {
+//     console.log('result', data);
+//   });
+
+// }).on("error", (err) => {
+//   console.log("Error: " + err.message);
+// });
